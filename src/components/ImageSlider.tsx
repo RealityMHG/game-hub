2521f1701253394d8d2fc15f09f81bf668be4b72
realitymhg <rafaelrego2000@gmail.com>
2024-12-11 01:29:45 +0000

    fix temporarly

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -1,6 +1,6 @@
 import getCroppedImageUrl from "../services/image-url";
 import { ScreenShot } from "../hooks/useGames";
-import { Button, Image } from "@chakra-ui/react";
+import { Box, Button, Image } from "@chakra-ui/react";
 import { MdNavigateNext, MdNavigateBefore } from "react-icons/md";
 import { useState } from "react";
 
@@ -8,22 +8,32 @@ interface Props {
   images: ScreenShot[];
 }
 
+//Fix the bug
 const ImageSlider = ({ images }: Props) => {
   const [imageIndex, setImageIndex] = useState(0);
   const showNextImage = () => {
-    setImageIndex((index) => (index == images.length - 1 ? 0 : index + 1));
+    setImageIndex((index) => (index == 0 ? images.length - 1 : index - 1));
   };
 
   const showPreviousImage = () => {
-    setImageIndex((index) => (index == 0 ? images.length - 1 : index - 1));
+    setImageIndex((index) => (index == images.length - 1 ? 0 : index + 1));
   };
 
   return (
     <>
+      <Box overflow="hidden" width="100%" height="100%" display="flex">
+        {images.map((url) => (
+          <Image
+            key={url.image}
+            src={getCroppedImageUrl(url.image)}
+            style={{ translate: `${-100 * imageIndex}%` }}
+            transition="translate 0.3s ease-in-out"
+          ></Image>
+        ))}
+      </Box>
       <Button className="image-btn" onClick={showNextImage} style={{ left: 0 }}>
         <MdNavigateBefore></MdNavigateBefore>
       </Button>
-      <Image src={getCroppedImageUrl(images[imageIndex].image)}></Image>
       <Button
         className="image-btn"
         onClick={showPreviousImage}
